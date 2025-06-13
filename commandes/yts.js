b618156ce5d780de97979aa26234e185946f6355
NjabuloJ <fanajb65@gmail.com>
2025-06-13 21:18:18 +0200

    Create yts.js

---
+++ commandes/yts.js
@@ -0,0 +1,76 @@
+
+const { fana } = require('../njabulo/fana');
+const axios = require("axios");
+const yts = require("yt-search");
+const fs = require('fs');
+const conf = require(__dirname + '/../set');
+
+fana({
+  nomCom: "yts",
+  categorie: "Search",
+  reaction: "🎼"
+}, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, arg } = commandeOptions;
+  const query = arg.join(" ");
+
+  if (!query) {
+     zk.sendMessage(dest, {
+      text: '😡Yo stop slacking! Give me a query, like .yts alone walk lost control',
+      contextInfo: {
+        externalAdReply: {
+          title: "Njabulo Jb",
+          body: "Message via ad !",
+          thumbnailUrl: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
+          sourceUrl: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
+          mediaType: 1,
+          showAdAttribution: true
+        }
+      }
+    });
+    return;
+  } 
+  try {
+    const info = await yts(query);
+    const results = info.videos;
+
+    if (results.length === 0) {
+      return repondre("No results found.");
+    }
+
+    let captions = `*${conf.BOT} YOUTUBE SEARCH*\n`;
+    results.slice(0, 10).forEach((video, index) => {
+      captions += `*────────────────────*\n${index + 1}.*Title:* ${video.title}\n*Time:* ${video.timestamp}\n*Url:* ${video.url}\n`;
+    });
+
+    captions += "\n─────────────────────\n*";
+
+    const thumb = results[0].thumbnail; // Using the first video's thumbnail
+
+    await zk.sendMessage(dest, {
+      image: { url: thumb },
+      caption: captions,
+      contextInfo: {
+         isForwarded: true,
+         forwardedNewsletterMessageInfo: {
+         newsletterJid: '120363345407274799@newsletter',
+         newsletterName: "NJABULO JB",
+         serverMessageId: 143,
+         },
+         forwardingScore: 999, // Score to indicate it has been forwarded
+         externalAdReply: {
+           title: `${conf.BOT} YouTube Search`,
+           body: `Top results for "${query}"`,
+           thumbnailUrl: 'https://files.catbox.moe/cs7xfr.jpg', // Add thumbnail URL if required 
+           sourceUrl: 'https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T', // Add source URL if necessary
+           mediaType: 1,
+           renderLargerThumbnail: true
+        },
+      },
+    }, { quoted: ms });
+
+  } catch (error) {
+    console.error("Error during the search process:", error);
+    repondre("Error during the search process: " + error.message);
+  }
+});
+￼Enter
