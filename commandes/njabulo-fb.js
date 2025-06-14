eb3a5a4030d1534b35538d6d29159ab44e7d3b83
NjabuloJ <fanajb65@gmail.com>
2025-06-14 18:03:51 +0200

    Create njabulo-fb.js

---
+++ commandes/njabulo-fb.js
@@ -0,0 +1,79 @@
+const {fana} = require('../njabulo/fana');
+const fs = require('fs');
+const getFBInfo = require("@xaviabot/fb-downloader");
+const { default: axios } = require('axios');
+
+fana({
+  nomCom: "fb2",
+  categorie: "Download",
+  reaction: "🎞️"
+},
+async (dest, zk, commandeOptions) => {
+  const { repondre, ms, arg } = commandeOptions;
+
+  if (!arg[0]) {
+    zk.sendMessage(dest, {
+      text: '😡Yo stop slacking! Give me a query, like .img cat',
+      contextInfo: {
+        externalAdReply: {
+          title: "Njabulo Jb",
+          body: "Message via ad !",
+          thumbnailUrl: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
+          sourceUrl: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
+          mediaType: 1,
+          showAdAttribution: true
+        }
+      }
+    });
+    return;
+  }
+
+  const queryURL = arg.join(" ");
+
+  try {
+     getFBInfo(queryURL)
+    .then((result) => {
+       let caption = `
+        titre: ${result.title}
+        Lien: ${result.url}
+      `;
+       zk.sendMessage(dest,{
+         image : { url : result.thumbnail}, 
+         caption : caption,
+            contextInfo: {
+        externalAdReply: {
+          title: "Njabulo Jb",
+          body: "Message via ad !",
+          thumbnailUrl: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
+          sourceUrl: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
+          mediaType: 1,
+          showAdAttribution: true
+              },
+            },
+          },{quoted : ms}) ;
+       zk.sendMessage(dest, { 
+         video: { url: result.sd  }, 
+         contextInfo: {
+        externalAdReply: {
+          title: "Njabulo Jb",
+          body: "Message via ad !",
+          thumbnailUrl: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
+          sourceUrl: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
+          mediaType: 1,
+          showAdAttribution: true
+           },
+         },
+       },{ quoted: ms });
+      
+    })
+    .catch((error) => {console.log("Error:", error)
+                      repondre(error)});
+
+
+   
+  } catch (error) {
+    console.error('Erreur lors du téléchargement de la vidéo :', error);
+    repondre('Erreur lors du téléchargement de la vidéo.' , error);
+  }
+});
+￼Enter
