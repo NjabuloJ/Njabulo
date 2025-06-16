f295a03af929a509747fd5b5b42fe8b94a7e9952
NjabuloJ <fanajb65@gmail.com>
2025-06-17 01:41:14 +0200

    Update ollmenu.js

--- commandes/ollmenu.js
+++ commandes/ollmenu.js
@@ -1,144 +1,96 @@
 const util = require('util');
 const fs = require('fs-extra');
+const axios = require('axios');
 const { fana } = require(__dirname + "/../njabulo/fana");
 const { format } = require(__dirname + "/../njabulo/mesfonctions");
 const os = require("os");
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
 
-fana({ nomCom: "ollmenu", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
-    let { cm } = require(__dirname + "/../njabulo//fana");
-    var coms = {};
-    var mode = s.MODE.toLowerCase() !== "yes" ? "private" : "public";
+const more = String.fromCharCode(8206);
+const readmore = more.repeat(4001);
 
-    cm.map(async (com) => {
-        if (!coms[com.categorie]) coms[com.categorie] = [];
-        coms[com.categorie].push(com.nomCom);
+fana({ nomCom: "me", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
+    let { cm } = require(__dirname + "/../njabulo/fana");
+
+    var commandsList = {};
+    var mode = (s.MODE).toLocaleLowerCase() !== "yes" ? "private" : "public";
+
+    cm.map((com) => {
+        if (!commandsList[com.categorie]) commandsList[com.categorie] = [];
+        commandsList[com.categorie].push(com.nomCom);
     });
 
-    moment.tz.setDefault("Africa/Nairobi");
-    const temps = moment().format('HH:mm:ss');
+    moment.tz.setDefault('EAT');
+
+    const time = moment().format('HH:mm:ss');
     const date = moment().format('DD/MM/YYYY');
 
-    // Generate greeting based on time of day
-    const hour = moment().hour();
-    let greeting = "🌅Good Morning my brother 🌄";
-    if (hour >= 12 && hour < 18) {
-        greeting = "🌄Good afternnon! Stay energized! 🌿";
-    } else if (hour >= 18) {
-        greeting = "🌇Good Everning! Hope you had a great day! 🌙";
-    } else if (hour >= 22 || hour < 5) {
-        greeting = "Good Night 🌌";
-    }
-
-    // Generate commands list
-    let commandList = "\n\nAvailable Commands";
-    for (let category in coms) {
-        commandList += `\n\n*${category}*\n`;
-        commandList += coms[category].map((cmd) => `- ${prefixe}${cmd}`).join("\n");
-    }
-
     let infoMsg = `
-╭══════════════⊷❍
-┇❍▸ ʙᴏᴛ ɴᴀᴍᴇ: *ᴄᴀsᴇʏʀʜᴏᴅᴇs ᴛᴇᴄʜ*
+╭━━✧𝐂𝐑𝐈𝐒𝐒  𝐕𝐌𝐃✧━━❖
+┊✺┌────••••────⊷
+┃✇│◎ 𝙾𝚠𝚗𝚎𝚛 : ${s.OWNER_NAME}
+┃✇│◎ 𝙿𝚛𝚎𝚏𝚒𝚡 : [ ${s.PREFIXE} ]
+┃✇│◎ 𝙼𝚘𝚍𝚎 : ${mode}
+┃✇│◎ 𝚁𝚊𝚖  : 8/132 GB
+┃✇│◎ 𝙳𝚊𝚝𝚎  : ${date}
+┃✇│◎ 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
+┃✇│◎ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : 𝐂𝐑𝐈𝐒𝐒 
+┃✇│◎ 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜 : ${cm.length}
+┃✇│ 𝚃𝚑𝚎𝚖𝚎 : 𝐕𝐄𝐕𝐎
+┊   └────••••────⊷
+╰━━✧𝐂𝐑𝐈𝐒𝐒  𝐕𝐌𝐃✧━━━◆ \n`;
 
-🌟 *${greeting}* 🌟
+    let menuMsg = ``;
 
-
-‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎
-
-${commandList}`;
-
-    // Two sets of images to display randomly
-    const extraImages1 = [
-        "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg",
-        "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg",
-        "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg"
-    ];
-
-    const extraImages2 = [
-        "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg",
-        "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg",
-        "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg"
-    ];
-
-    // Randomly select which menu to show
-    const isOriginalMenu = Math.random() > 0.5; // 50% chance for either menu
-
-    let mediaUrl, thumbnail, renderType;
-    if (isOriginalMenu) {
-        mediaUrl = mybotpic(); // Use bot’s original picture
-        thumbnail = extraImages1[Math.floor(Math.random() * extraImages1.length)];
-        renderType = "renderLargerThumbnail";
-    } else {
-        mediaUrl = extraImages2[Math.floor(Math.random() * extraImages2.length)];
-        thumbnail = mediaUrl; // Use the same image as media
-        renderType = "renderSmallThumbnail";
-    }
-
-    try {
-        if (mediaUrl.match(/\.(mp4|gif)$/i)) {
-            await zk.sendMessage(dest, {
-                video: { url: mediaUrl },
-                caption: infoMsg,
-                footer: "Jb",
-                gifPlayback: true,
-                contextInfo: {
-                    externalAdReply: {
-                        title: "Njabulo Jb",
-                        body: "Tap here to Join our official channel!",
-                        mediaType: 1,
-                        thumbnailUrl: thumbnail,
-                        sourceUrl: "https://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E",
-                        showAdAttribution: true,
-                        [renderType]: true, // Apply correct thumbnail size
-                    },
-                },
-            }, { quoted: ms });
-        } else {
-            await zk.sendMessage(dest, {
-                image: { url: mediaUrl },
-                caption: infoMsg,
-                footer: "Jb",
-                contextInfo: {
-                    externalAdReply: {
-                        title: "Njabulo Jb",
-                        body: "Tap here to Join our official channel!",
-                        mediaType: 1,
-                        thumbnailUrl: thumbnail,
-                        sourceUrl: "https://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E",
-                        showAdAttribution: true,
-                        [renderType]: true, // Apply correct thumbnail size
-                    },
-                },
-            }, { quoted: ms });
+    for (const category in commandsList) {
+        menuMsg += `
+╭━━━❂ ${category} ❂⁠⁠⁠⁠━━─••
+║╭━━══••══━━••⊷ `;
+        for (const cmd of commandsList[category]) {
+            menuMsg += `          
+║┊◆ ${s.PREFIXE}  *${cmd}*`;
         }
-    } catch (e) {
-        console.log("🥵🥵 Error sending menu: " + e);
-        repondre("🥵🥵 Error sending menu: " + e);
+        menuMsg += `
+║╰━━══••══━━••⊷
+╰════────════◆◆◆`;
     }
 
-    // List of audio URLs
-    const audioUrls = [
-        "https://files.catbox.moe/wsyxi0.mp3",
-        "https://files.catbox.moe/w2k8g2.mp3",
-        "https://files.catbox.moe/cpjbnl.mp3",
-        "https://files.catbox.moe/y6fph9.mp3",
-        "https://files.catbox.moe/moctzu.mp3" // New song added
-    ];
+    menuMsg += `\n> @𝐌𝐀𝐃𝐄 𝐁𝐘 𝐂𝐑𝐈𝐒𝐒 𝐕𝐄𝐕𝐎`;
 
-    // Select a random audio file
-    const randomAudioUrl = audioUrls[Math.floor(Math.random() * audioUrls.length)];
+    var imageUrl = mybotpic();
 
     try {
-        await zk.sendMessage(dest, {
-            audio: { url: randomAudioUrl },
-            mimetype: 'audio/mpeg',
-            ptt: true, // Send as a voice note
-        }, { quoted: ms });
+        if (imageUrl.match(/\.(mp4|gif)$/i)) {
+            await zk.sendMessage(dest, { caption: infoMsg + menuMsg, gifPlayback: true }, { quoted: ms });
+        } else if (imageUrl.match(/\.(jpeg|png|jpg)$/i)) {
+            await zk.sendMessage(dest, { caption: infoMsg + menuMsg }, { quoted: ms });
+        } else {
+            repondre(infoMsg + menuMsg);
+        }
+
+        // Download and send audio
+        const audioUrl = "https://files.catbox.moe/xci982.mp3";
+        const audioPath = "./temp_audio.mp3";
+
+        const response = await axios({
+            url: audioUrl,
+            method: "GET",
+            responseType: "stream",
+        });
+
+        const writer = fs.createWriteStream(audioPath);
+        response.data.pipe(writer);
+
+        writer.on("finish", async () => {
+            await zk.sendMessage(dest, { audio: { url: audioPath }, mimetype: "audio/mp4", ptt: true }, { quoted: ms });
+            fs.unlinkSync(audioPath); // Delete the audio file after sending
+        });
+
     } catch (e) {
-        console.log("🥵🥵 Error sending audio: " + e);
-        repondre("🥵🥵 Error sending audio: " + e);
+        console.log("🥵🥵 Menu error: " + e);
+        repondre("🥵🥵 Menu error: " + e);
     }
 });
+￼Enter
