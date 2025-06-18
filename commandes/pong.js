96a30b8d5e0acae2074edd7a61d2d568a7b92e76
NjabuloJ <fanajb65@gmail.com>
2025-06-19 00:39:24 +0200

    Update pong.js

--- commandes/pong.js
+++ commandes/pong.js
@@ -27,12 +27,12 @@ fana({ nomCom: "pong", categorie: "General" }, async (dest, zk, commandeOptions)
 
     try {
         await zk.sendMessage(dest, { 
-            text: `*🚀 SYSTEM PING* 🔋\n\n🕒 System Time: ${time}\n*▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰*\n⚡ Response: ${ping}ms🟢\n📶 Quality: HIGH LATENCY\nrun online\n- -run: 99.${ping}%\n- -cmd: ${ping}1%\n\n🌐 Routing: Automatic Optimization\n🖥️ Server: Core-: ${date} :${ping}\n*▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰*\n> *ping is available working online 24/7*`,
+            text: `> *🚀 SYSTEM PING* 🔋\n\n🕒 System Time: ${time}\n*▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰*\n🟢Response: ${ping}ms\n📶 Quality: HIGH LATENCY\n- -run online\n- -run: 99.${ping}%\n- -cmd: ${ping}1%\n\n🌐 Routing: Automatic Optimization\n🖥️ Server: Core-: ${date} :${ping}\n*▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰*\n> *ping is available working online 24/7*`,
             contextInfo: {
           isForwarded: true,
          forwardedNewsletterMessageInfo: {
          newsletterJid: '120363399999197102@newsletter',
-         newsletterName: "pong",
+         newsletterName: "",
          serverMessageId: 143,
                 }
             }
